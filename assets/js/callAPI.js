83f26c10e27cb348501b9e7a1a1f3c8e5bd3cebe
oliverlkelly <18181648+oliverlkelly@users.noreply.github.com>
2022-01-13 19:17:32 +1100

    added button to HTML for testing api call,  set up javascript file to point to button and put api key and call url in

---
+++ assets/js/callAPI.js
@@ -0,0 +1,10 @@
+var searchBtn = document.querySelector("#searchBtn");
+var apiKey = "58cbc000b937473796ed1b4bcb46acc1"
+var apiURL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=58cbc000b937473796ed1b4bcb46acc1"
+var pantry = ["Milk", "Eggs", "Bread"];
+
+searchBtn.addEventListener("click", callAPI);
+
+function callAPI(foodItems){
+    
+}
